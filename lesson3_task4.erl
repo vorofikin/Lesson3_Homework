1fe017c1beb16e7244ef8732818a9507038d1169
Andrew <nikiforov564@gmail.com>
2023-09-02 22:27:57 +0300

    added task 4

---
+++ lesson3_task4.erl
@@ -0,0 +1,99 @@
+-module(lesson3_task4).
+
+-export([decode/2]).
+
+-define(SINGLE_QUOTE, "'").
+-define(COMMA, ",").
+-define(OPEN_CURLY_BRACE, "{").
+-define(CLOSE_CURLY_BRACE, "}").
+-define(COLON, ":").
+-define(OPEN_SQUARE_BRACKET, "[").
+-define(CLOSE_SQUARE_BRACKET, "]").
+-define(NEW_LINE, "\n").
+-define(TRUE, "true").
+-define(FALSE, "false").
+-define(IS_SPACE(Char),
+  Char == 10;
+  Char == 32
+).
+
+decode(Json, Format) ->
+  case Format of
+    proplist -> decode_proplist(Json, []);
+    map -> decode_map(Json, maps:new());
+    _ -> undefined
+  end.
+
+decode_proplist(<<?COMMA, Rest/binary>>, PropList) ->
+  {Key, Value, Rest1} = tokenize(Rest, <<>>, <<>>, proplist),
+  decode_proplist(Rest1, [{Key, Value} | PropList]);
+decode_proplist(<<?OPEN_CURLY_BRACE, Rest/binary>>, PropList) ->
+  {Key, Value, Rest1} = tokenize(Rest, <<>>, <<>>, proplist),
+  decode_proplist(Rest1, [{Key, Value} | PropList]);
+decode_proplist(<<Char, Rest/binary>>, PropList) when ?IS_SPACE(Char) ->
+  decode_proplist(Rest, PropList);
+decode_proplist(<<?CLOSE_CURLY_BRACE>>, PropList) ->
+  lists:reverse(PropList);
+decode_proplist(<<?CLOSE_CURLY_BRACE, Rest/binary>>, PropList) ->
+  {lists:reverse(PropList), Rest}.
+
+decode_map(<<?COMMA, Rest/binary>>, Map) ->
+  {Key, Value, Rest1} = tokenize(Rest, <<>>, <<>>, map),
+  decode_map(Rest1, maps:put(Key, Value, Map));
+decode_map(<<?OPEN_CURLY_BRACE, Rest/binary>>, Map) ->
+  {Key, Value, Rest1} = tokenize(Rest, <<>>, <<>>, map),
+  decode_map(Rest1, maps:put(Key, Value, Map));
+decode_map(<<Char, Rest/binary>>, Map) when ?IS_SPACE(Char) ->
+  decode_map(Rest, Map);
+decode_map(<<?CLOSE_CURLY_BRACE>>, Map) ->
+  Map;
+decode_map(<<?CLOSE_CURLY_BRACE, Rest/binary>>, Map) ->
+  {Map, Rest}.
+
+tokenize(<<?SINGLE_QUOTE, Rest/binary>>, <<>>, <<>> = Value, Flag) ->
+  {Key, Rest1} = tokenize_name(Rest, <<>>),
+  tokenize(Rest1, Key, Value, Flag);
+tokenize(<<?SINGLE_QUOTE, Rest/binary>>, Key, <<>>, _) ->
+  {Value, Rest1} = tokenize_name(Rest, <<>>),
+  {Key, Value, Rest1};
+tokenize(<<?COLON, Rest/binary>>, Key, Value, Flag) ->
+  tokenize(Rest, Key, Value, Flag);
+tokenize(<<?OPEN_SQUARE_BRACKET, Rest/binary>>, Key, <<>>, Flag) ->
+  {Value, Rest1} = tokenize_list(Rest, [], Flag),
+  {Key, Value, Rest1};
+tokenize(<<Char, Rest/binary>>, Key, Value, Flag) when ?IS_SPACE(Char) ->
+  tokenize(Rest, Key, Value, Flag);
+tokenize(<<?TRUE , Rest/binary>>, Key, <<>>, _) ->
+  {Key, true, Rest};
+tokenize(<<?FALSE , Rest/binary>>, Key, <<>>, _) ->
+  {Key, false, Rest};
+tokenize(Rest, Key, <<>>, _) ->
+  {Value, Rest1} = tokenize_number(Rest, <<>>),
+  {Key, Value, Rest1}.
+
+tokenize_list(<<?OPEN_CURLY_BRACE, _/binary>> = Rest, List, Format) ->
+  {Value, Rest1} =
+    case Format of
+      map -> decode_map(Rest, maps:new());
+      proplist -> decode_proplist(Rest, [])
+    end,
+  tokenize_list(Rest1, [Value | List], Format);
+tokenize_list(<<?SINGLE_QUOTE, Rest/binary>>, List, Flag) ->
+  {Name, Rest1} = tokenize_name(Rest, <<>>),
+  tokenize_list(Rest1, [Name | List], Flag);
+tokenize_list(<<?COMMA, Rest/binary>>, List, Flag) ->
+  tokenize_list(Rest, List, Flag);
+tokenize_list(<<?CLOSE_SQUARE_BRACKET, Rest/binary>>, List, _) ->
+  {List, Rest};
+tokenize_list(<<Char, Rest/binary>>, List, Flag) when ?IS_SPACE(Char) ->
+  tokenize_list(Rest, List, Flag).
+
+tokenize_number(<<?COMMA, _/binary>> = Rest, Number) ->
+  {binary_to_integer(Number), Rest};
+tokenize_number(<<Char:1/binary, Rest/binary>>, Number) ->
+  tokenize_number(Rest, <<Number/binary, Char/binary>>).
+
+tokenize_name(<<?SINGLE_QUOTE, Rest/binary>>, Name) ->
+  {Name, Rest};
+tokenize_name(<<Char, Rest/binary>>, Name)->
+  tokenize_name(Rest, <<Name/binary, Char>>).
